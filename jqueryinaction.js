b88cac6d3c989505e497ae86bc9bdf420faa356a
Can Bilgin <canbilgin.f@gmail.com>
2017-08-21 21:35:29 +0300

    The last change was the addition of the reset button.

--- jqueryinaction.js
+++ jqueryinaction.js
@@ -1,7 +1,28 @@
 $(document).ready(function()
 {
-  $("button.Apply_button").click(function(){
-    $("#submit_button").css("color","red");
+  var pad_array = [];
+  $("button").click(function(){
+  //  $("#submit_button").css("color","red");
     $("li").css("background-color","red");
+    $(this).css("background-color","red");
+    var elements = $("#fill_me").val();
+    $("#input_text").val(elements) = pad_array;
   });
+  $("div").mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });7
+   $('div').mouseleave(function(){
+     $(this).animate({
+       height: '-=10px'
+
+     });
+   });
+   $("input:checkbox").click(function(){
+     $("li").css("color","orange");
+   });
+   $("#reset_button").click(function(){
+   window.location.reload();
+ });
 });
