8bf90128e69824f727a31409fee0a58005cc7167
Can Bilgin <canbilgin.f@gmail.com>
2017-09-07 20:47:24 +0300

    The old files are deleted

--- html,css,js/the jquery file behind jquery file.js
+++
@@ -1,7 +0,0 @@
-window.alert("CSS1 sayfasına geçiş");
-$(document).ready(function(){
-  $("<div class='foo'> I have foo!</div><div>I don't</div>")
-  .filter(".foo").click(function() {
-    alert("I'm foo!");
-  }).end().appendTo("#someParentDiv")
-  });
